9463286b3cc8a865025616181951c2c29b1ced43
Sopho Chitadze <chitadzesopho2@gmail.com>
2024-01-29 23:23:30 +0400

    isn't done

---
+++ main.js
@@ -0,0 +1,74 @@
+// function exercise1() {
+//     let number = prompt("Enter a number: ");
+//     let result = "";
+
+//     for (let i = 0; i < number; i++) {
+//         for (let j = 0; j < number; j++) {
+//             result += "*";
+//         }
+//         result += "\n";
+//     }
+//     alert(result);
+// }
+
+// exercise1()
+
+// function exercise2() {
+//     function isPrime(n) {
+//         for (let i = 2; i < n; i++) {
+//             if (n % i === 0) {
+//                 return false;
+//             }
+//         }
+//         return n > 1;
+//     }
+//     console.log(isPrime(13));
+// }
+
+// let number = prompt("Enter a number: ");
+// let result = "";
+// for (let i = 2; i <= number; i++) {
+//     if (isPrime(i)) {
+//         result += $(i);
+//     }
+// }
+// exercise2()
+
+
+// function exercise3() {
+//     let calculator = {
+//         read: function () {
+//             this.number1 = prompt("Enter a number");
+//             this.number2 = prompt("Enter another number");
+//         }
+//         sum: function () {
+//             return this.number1 + this.number2;
+//         }
+//         nul: function () {
+//             return this.number1 * this.number2; 
+//         }
+//     }
+//     calculator.read();
+//     alert(calculator.sum());
+//     alert(calculator.nul());
+// }
+// exercise3();
+
+function exercise4() {
+    let ladder = {
+        step: 0,
+        up() {
+            this.step++;
+            return this;
+        },
+        down() {
+            this.step--;
+            return this;
+        },
+        showStep() {
+            alert(this.step);
+            return this;
+        }
+    }
+}
+exercise4();
